e1db1ab5e13427aa1c03c29b64cc691158228e2e
2021wc86664 <2021wc8664@wilp.bits-pilani.ac.in>
2025-07-14 20:39:35 +0530

    App updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven Project!");
+	System.out.println("App Updated");
     }
 
     public static int add(int a, int b) {
